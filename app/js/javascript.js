e025479176a56559de0be51db95f5d14fa700eb1
Andy <rym1112ov@gmail.com>
2016-12-18 16:11:18 +0200

    fix animation

--- app/js/javascript.js
+++ app/js/javascript.js
@@ -3,9 +3,7 @@ document.getElementById('hamburger').addEventListener('click', function() {
     document.querySelector('body').classList.toggle('active');
     
 });
-var timeInt = setInterval(function(){
-	document.getElementById('footprint').classList.toggle('_footsteps-active');
-	
-},3001); 
 
+
+	
   	
